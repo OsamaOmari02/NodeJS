c4fd57c2cd9c85fe3c1d1a770286b0c8fde42cf4
OsamaOmari02 <osama.omarii02@gmail.com>
2022-11-23 02:56:46 +0200

    section 12 vid 200 mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,28 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const { MongoClient } = mongodb;
+/** @type {mongodb.Db} */
+let _db;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://osama:osama123456@cluster0.s7arlfa.mongodb.net/shop?retryWrites=true&w=majority')
+    .then(client => {
+      console.log('DB connected!!!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err
+    });
+}
 
-module.exports = sequelize;
+const getDb = () => {
+  if (_db)
+    return _db;
+  throw 'No DB found !!'
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+
+
